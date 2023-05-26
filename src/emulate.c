00ed7d908fa714ac35bc1c8c07204aa98876a2df
Bogdan Gavra <bg622@ic.ac.uk>
2023-05-26 16:45:33 +0100

    Group effort: Reading 32-bit instruction from bin file functionality implemented

--- src/emulate.c
+++ src/emulate.c
@@ -1,5 +1,42 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-int main(int argc, char **argv) {
-  return EXIT_SUCCESS;
+#define BUFSZ 4
+#define BYTESZ 8
+
+bool read_word(FILE *fptr, int32_t *word)
+{
+	return fread(word, sizeof(int), 1, fptr) == 1;
+//	return fread(buf, size, nmemb, fptr) == BUFSZ;
+}
+
+int32_t read_word_old(unsigned char *buf)
+{
+	return *(int32_t*)buf;
+/*	int32_t word = 0;
+	for(int i=0;i<BUFSZ;i++)
+	{
+		word = (word << BYTESZ) + buf[i];
+	}
+	return word;
+*/
+}
+
+int main(int argc, char **argv) 
+{
+	FILE* fptr = fopen(argv[1], "r");
+	if(fptr == NULL)
+	{
+		printf("File could not be opened");
+		exit(1);
+	}
+	int32_t word;
+	while(read_word(fptr, &word))
+	{
+		//to:do Add opcode checking
+		printf("0x%x\n", word);
+	}
+	fclose(fptr);
+	return 0;
 }
