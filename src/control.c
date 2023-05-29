8bd2fd00161cab6b24024d95eceb58a933b54797
Bogdan Gavra <bobi.gavra@gmail.com>
2023-05-29 17:16:25 +0100

    Special instructions functionality added

--- src/control.c
+++ src/control.c
@@ -3,26 +3,37 @@
 #include <stdio.h>
 
 // Returns bits at the interval <start, end> (interval is closed).
-int getBits(int end, int start, int instruction){
+int getBits(int end, int start, int instruction)
+{
     return (instruction &  ((1 << (end + 1)) - (1 << start))) >> end;
 }
 
 // Returns type of the instruction
-instructionType getInstructionType(int instruction){
+instructionType getInstructionType(int32_t instruction)
+{
     int bitmasks[] = {0b1000, 0b0101, 0b0100, 0b1010};
     int dontCares[] = {0b1110, 0b0111, 0b0101, 0b1110};
     int opcode = getBits(28, 25, instruction);
     instructionType type = UNDEFINED;
 
-    for (instructionType i = FIRST; i <= LAST; i++){
-        if ((dontCares[i] & opcode) == bitmasks[i]){
+    for (instructionType i = FIRST; i <= LAST; i++)
+    {
+        if ((dontCares[i] & opcode) == bitmasks[i])
+        {
             type = i;
         }
     }
     return type;
 }
 
-void ExecuteInstruction(int instruction, ComputerState *computerState){
+void ExecuteInstruction(int32_t instruction, ComputerState *computerState)
+{
+
+    //Special instructions
+    if(ExecuteSpecialInstruction(instruction, computerState))
+        return;
+
+    //Normal instuctions
     switch (getInstructionType(instruction)) {
         case IMMEDIATE:
             //ExecuteImmediate(instruction, computerState);
@@ -41,3 +52,16 @@ void ExecuteInstruction(int instruction, ComputerState *computerState){
         }
 }
 
+bool ExecuteSpecialInstruction(int32_t instructionType, ComputerState* computerState) 
+{
+    switch(instructionType) {
+        case 0xd503201f: // NOP
+            computerState->PC += 4;
+            break;
+        case 0x8a000000: // Halt
+
+        default:
+            return false;
+    }
+    return true;
+}
