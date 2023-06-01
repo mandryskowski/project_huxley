90333eefef91c2b517745b4127dcf5fba8a40cb7
ma4722 <ma4722@ic.ac.uk>
2023-06-01 16:38:54 +0100

    Introduced the idea of operation type enums

--- src/assembleDPI.h
+++ src/assembleDPI.h
@@ -1,6 +1,28 @@
 #ifndef ASSEMBLEDPI_H
 #define ASSEMBLEDPI_H
 
-int32_t assembleDPI(char* line);
+typedef enum
+{
+	DP_ADD,
+	DP_ADDS,
+	DP_SUB,
+	DP_SUBS,
+	DP_AND,
+	DP_ANDS,
+	DP_BIC,
+	DP_BICS,
+	DP_EOR,
+	DP_EON,
+	DP_ORR,
+	DP_ORN,
+	DP_MOVN,
+	DP_MOV_UNDEFINED,
+	DP_MOVK,
+	DP_MOVZ,
+	DP_MADD,
+	DP_MSUB
+} DPOperation;
+
+int32_t assembleDPI(char* str, DPOperation);
 
 #endif //ASSEMBLEDPI_H
