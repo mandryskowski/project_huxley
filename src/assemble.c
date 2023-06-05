31ee0fff94b18e9bc038df220cac55e1bb1294b5
Jakub Lapinski <jakub.lapinski22@imperial.ac.uk>
2023-06-05 17:36:20 +0200

    assemble.c finally works(needs check for memory leaks tho)

--- src/assemble.c
+++ src/assemble.c
@@ -15,18 +15,19 @@ void makeStrLowercase(char* str)
 }
 
 // Replace all tokens matching a label with their corresponding addresses. Convert the rest to lowercase.
-void processLabelTokens(char** tokens, Label* labels)
+void processLabelTokens(char*** tokenized, char** tokens, Label* labels, char **toFreeEnd)
 {
     while (*tokens != NULL)
     {
         uint64_t address = getLabelAddress(*tokens, labels);
-        //printf("address of token %s: %d \n", *tokens, address);
         if (address == MAX_UINT64T) // if this is not a label, make it lowercase.
         {
             makeStrLowercase(*tokens);
         }
         else
         {
+            *tokens = calloc(1, 19);
+            *toFreeEnd++ = *tokens;
             sprintf(*tokens, "#%" PRIu64, address); // replace label token with #address.
         }
         tokens++;
@@ -48,12 +49,15 @@ int main(int argc, char **argv)
     int fileSize = getFileSize(input);
     char *data = malloc(fileSize + 1);
     fread(data, sizeof(char), fileSize, input);
+
     char ***tokenized = calloc(sizeof(char **) * (fileSize + 1), 1);
     char ***tokenizedPtr = tokenized;
-    uint64_t address = 0;
+    char *line;
+    char **toFree = calloc(512, sizeof(char **));
+    char **toFreeEnd = toFree;
     Label *label = calloc(128, sizeof(Label));
     Label *labelEnd = label;
-    char *line;
+    uint64_t address = 0;
 
     // First pass
     while ((line = strsep(&data, "\n")))
@@ -64,6 +68,10 @@ int main(int argc, char **argv)
             continue;
         }
         *tokenizedPtr = split(line);
+        if (!(**tokenizedPtr))
+        {
+            continue;
+        }
         char *operation = **tokenizedPtr;
         int opLength = strlen(operation);
         // Is a label
@@ -83,18 +91,24 @@ int main(int argc, char **argv)
     // Second pass
     while (*tokenizedPtr)
     {
-        processLabelTokens(*tokenizedPtr, label);
+        processLabelTokens(tokenizedPtr, *tokenizedPtr, label, toFreeEnd);
         uint32_t word = assembleInstruction(*tokenizedPtr, address);
         for (int i = 0; i < 32; i += 8)
         {
             fprintf(output, "%c", (unsigned char) (word >> i));
         }
 
-        //printf("%08x\n", word);
         address += 4;
         tokenizedPtr++;
     }
 
+    toFreeEnd = toFree;
+    while (*toFreeEnd)
+    {
+        free(*toFreeEnd);
+        toFreeEnd++;
+    }
+    free(toFree);
     free(data);
     free(tokenized);
     free(label);
