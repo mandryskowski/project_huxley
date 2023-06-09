c8a2448f46678e8b0ca9e41ece73b4ca81538ee2
ma4722 <ma4722@ic.ac.uk>
2023-06-09 18:23:10 +0100

    gui moved to separate file, basic entity debugging

--- extension/render.h
+++ extension/render.h
@@ -1,6 +1,7 @@
 #ifndef RENDER_H
 #define RENDER_H
 #include "state.h"
+#include <stdbool.h>
 typedef unsigned int uint;
 
 typedef struct RenderState
@@ -9,6 +10,8 @@ typedef struct RenderState
     uint QuadVAO;
     int shader;
     uint tileAtlas, characterAtlas;
+
+    bool bDebugHitboxes;
 } RenderState;
 
 void initRenderState(GameState*, RenderState*);
