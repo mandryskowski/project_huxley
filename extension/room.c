a5f0532d9bb55ea824df0485312e5ece29e8484b
Jakub Lapinski <jakub.lapinski22@imperial.ac.uk>
2023-06-20 18:48:22 +0100

    working on item in progress

--- extension/room.c
+++ extension/room.c
@@ -11,6 +11,28 @@ TileType getTile(Vec2i vec, GameState *state)
     return state->currentLevel->currentRoom->tiles[vec.x][vec.y].type;
 }
 
+int tile_type_to_textureID(TileType tileType, RoomType roomType, int x, int y, Room *room)
+{
+    switch (tileType)
+    {
+        case TILE_FLOOR:
+            return 12;
+        case TILE_BARRIER:
+            return roomType == ITEM_ROOM ? 11 : 15;
+        case TILE_DOOR:
+        {
+            int non_zero = x && x != room->size.x - 1 ? x - room->size.x / 2 : y - room->size.y / 2;
+            return non_zero ? 9 : 10;
+        }
+        case TILE_HOLE:
+            return 8;
+        case TILE_WALL:
+            return 13;
+        default:
+            return 0;
+    }
+}
+
 Room *construct_room(char *filename , RoomType type)
 {
     FILE *file = fopen(filename, "r");
@@ -23,15 +45,16 @@ Room *construct_room(char *filename , RoomType type)
     room->entity_cnt = 1;
     room->tiles = malloc(width * sizeof(Tile*));
     room->visited = false;
+    room->type = type;
 
     for (int x = 0; x < width; x++)
     {
         room->tiles[x] = malloc(height * sizeof(Tile));
         for (int y = 0; y < height; y++)
         {
-            TileType type;
-            fscanf(file, "%d", &type);
-            room->tiles[x][y] = (Tile){.textureID = 0, .type = type};
+            TileType tileType;
+            fscanf(file, "%d", &tileType);
+            room->tiles[x][y] = (Tile){.textureID = tile_type_to_textureID(tileType, type, x, y, room) , .type = tileType};
         }
     }
 
@@ -41,13 +64,18 @@ Room *construct_room(char *filename , RoomType type)
     {
         for (int y = 0; y < height; y++)
         {
-            MonsterType type;
-            fscanf(file, "%d", &type);
-            if (type != NOT_MONSTER)
+            MonsterType monsterType;
+            fscanf(file, "%d", &monsterType);
+            if (monsterType != NOT_MONSTER)
             {
-                room->entities[room->entity_cnt++] = construct_monster((Vec2d){x + 0.5, y + 0.5}, type, room);
+                room->entities[room->entity_cnt++] = construct_monster((Vec2d){x + 0.5, y + 0.5}, monsterType, room);
             }
         }
+    }
+
+    if (room->type == ITEM_ROOM)
+    {
+        room->entities[room->entity_cnt++] =
     }
 
     fclose(file);
