c1036e714e041f16d173a63f09e1d727a933dcde
Jakub Lapinski <jakub.lapinski22@imperial.ac.uk>
2023-06-23 00:12:58 +0100

    library for audio added

---
+++ extension/java.c
@@ -0,0 +1,5 @@
+//
+// Created by Jakub Łapiński on 22/06/2023.
+//
+
+#include "java.h"
---
+++ extension/java.h
@@ -0,0 +1,8 @@
+//
+// Created by Jakub Łapiński on 22/06/2023.
+//
+
+#ifndef ARMV8_25_JAVA_H
+#define ARMV8_25_JAVA_H
+
+#endif //ARMV8_25_JAVA_H
